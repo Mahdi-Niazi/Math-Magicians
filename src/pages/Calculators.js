e5ffb9b29e1b72c9c5b9e363c5ae1c6899b829d5
Mohammad Mahdi Niazi <m.mahdi.niazi@gmail.com>
2023-08-15 22:28:10 +0430

    Add full website functionality Add React route Add Navbar Add style to Navbar Add home page Add style to home page

---
+++ src/pages/Calculators.js
@@ -0,0 +1,111 @@
+import React, { useState } from 'react';
+import calculate from '../logic/calculate';
+
+const Calculators = () => {
+  const [state, setState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+  const { total, operation, next } = state;
+  const handleEvent = (e) => {
+    const value = e.target.textContent;
+    const calculat = calculate(state, value);
+    setState(calculat);
+  };
+  return (
+    <>
+      <div className="container-calc">
+        <h1>Let&apos;s do some maths</h1>
+        <div>
+          <div className="input">
+            {total}
+            {operation}
+            {next}
+          </div>
+          <div className="parent">
+            <button type="button" className="button" onClick={handleEvent}>
+              AC
+            </button>
+            <button type="button" className="button" onClick={handleEvent}>
+              +/-
+            </button>
+            <button type="button" className="button" onClick={handleEvent}>
+              %
+            </button>
+            <button
+              type="button"
+              className="button-color"
+              onClick={handleEvent}
+            >
+              ÷
+            </button>
+            <button type="button" className="button" onClick={handleEvent}>
+              7
+            </button>
+            <button type="button" className="button" onClick={handleEvent}>
+              8
+            </button>
+            <button type="button" className="button" onClick={handleEvent}>
+              9
+            </button>
+            <button
+              type="button"
+              className="button-color"
+              onClick={handleEvent}
+            >
+              x
+            </button>
+            <button type="button" className="button" onClick={handleEvent}>
+              4
+            </button>
+            <button type="button" className="button" onClick={handleEvent}>
+              5
+            </button>
+            <button type="button" className="button" onClick={handleEvent}>
+              6
+            </button>
+            <button
+              type="button"
+              className="button-color"
+              onClick={handleEvent}
+            >
+              -
+            </button>
+            <button type="button" className="button" onClick={handleEvent}>
+              1
+            </button>
+            <button type="button" className="button" onClick={handleEvent}>
+              2
+            </button>
+            <button type="button" className="button" onClick={handleEvent}>
+              3
+            </button>
+            <button
+              type="button"
+              className="button-color"
+              onClick={handleEvent}
+            >
+              +
+            </button>
+            <button type="button" className="button-zero" onClick={handleEvent}>
+              0
+            </button>
+            <button type="button" className="button" onClick={handleEvent}>
+              .
+            </button>
+            <button
+              type="button"
+              className="button-color"
+              onClick={handleEvent}
+            >
+              =
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Calculators;
---
+++ src/pages/Quote.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+
+const apiUrl = 'https://api.api-ninjas.com/v1/quotes?category=education';
+const apiKey = 'bE08nSscaJlysmr9PF2nRA==Rf2z5asHSprjI7m4';
+
+const Quote = () => {
+  const [post, setPost] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(apiUrl, {
+          headers: { 'X-Api-Key': apiKey },
+        });
+        if (res.ok) {
+          const data = await res.json();
+          const dataBody = `${data[0].quote} - ${data[0].author}`;
+          setPost(dataBody);
+        } else {
+          throw new Error('Error Loading Data');
+        }
+      } catch (error) {
+        setError('Error Loading Data');
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <div className="quotes">
+      {loading && <p> Loading...</p>}
+      {error && <p>{error}</p>}
+      {!loading && !error && (
+        <div>
+          <p>{post}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Quote;
